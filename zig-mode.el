7f7a67de90fa973884321a0bfa2f15b12af3919c
JenChieh <jcs090218@gmail.com>
2024-02-21 02:08:13 -0800

    fix: Symbol as function name

--- zig-mode.el
+++ zig-mode.el
@@ -479,10 +479,10 @@ This is written mainly to be used as `end-of-defun-function' for Zig."
 
 (defvar zig-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "C-c C-b") 'zig-compile)
-    (define-key map (kbd "C-c C-f") 'zig-format-buffer)
-    (define-key map (kbd "C-c C-r") 'zig-run)
-    (define-key map (kbd "C-c C-t") 'zig-test-buffer)
+    (define-key map (kbd "C-c C-b") #'zig-compile)
+    (define-key map (kbd "C-c C-f") #'zig-format-buffer)
+    (define-key map (kbd "C-c C-r") #'zig-run)
+    (define-key map (kbd "C-c C-t") #'zig-test-buffer)
     map)
   "Keymap for Zig major mode.")
 
